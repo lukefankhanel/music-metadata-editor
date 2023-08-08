2cc5f49b94536609803e8eee344cdb1c9d7bf31b
lukefankhanel <lukerfankhanel@gmail.com>
2023-08-08 16:15:56 -0600

    MP4 Implementation

--- src/audiotypes.py
+++ src/audiotypes.py
@@ -1,9 +1,8 @@
 
 from abc import ABC, abstractmethod
+from mutagen.flac import FLAC
 from mutagen.oggopus import OggOpus
 from mutagen.mp4 import MP4
-from mutagen.mp3 import MP3
-from mutagen.flac import FLAC
 from mutagen.id3 import ID3, TRCK, TPOS, TIT2, TPE1, TALB, TDRC, TCON, TCOM, WXXX, TXXX, COMM, TIT1
 
 
@@ -18,7 +17,6 @@ def createFileObject(fileLocation):
         return MP3File(fileLocation)
     else:
         return None
-
 
 
 class SongFile(ABC):
@@ -149,38 +147,115 @@ class OGGFile(SongFile):
     def setURL(self, value):
         self.setMetadataField("purl", value)
 
-    """
-    https://datatracker.ietf.org/doc/html/rfc7845.html
-    https://en.wikipedia.org/w/index.php?title=Q_%28number_format%29&oldid=697252615
-    https://hydrogenaud.io/index.php/topic,119279.0.html
-    This is a difficult issue to fix. The value is stored inside the ID header's 'output gain'
-    field. As such, with Mutagen, there appears to be no way to access and change this field.
-    The R128_Track/Album_Gain fields are used to DERIVE the value of the gain from the
-    afformentioned output gain field. The output gain stores the actual DISC replay gain.
-    Then, to get the TRACK gain, you take the difference of the output gain fields and the
-    R128_TRACK_GAIN field. In most cases, the R128_ALBUM_GAIN field is not used.
-    I've tried just setting the Track Gain field regardless without having access to the
-    ID headers, to no avail. All in all, such a stupid, unintuitive rediculous design choice.
-    I have no idea why you would go against what literally every other codec does just to be
-    "fancy". Dumb. This could be fixed, but you'd have to use another library that allows for
-    the setting of the ID headers. 
-    """
+    
+    # https://datatracker.ietf.org/doc/html/rfc7845.html
+    # https://en.wikipedia.org/w/index.php?title=Q_%28number_format%29&oldid=697252615
+    # https://hydrogenaud.io/index.php/topic,119279.0.html
+    # This is a difficult issue to fix. The value is stored inside the ID header's 'output gain'
+    # field. As such, with Mutagen, there appears to be no way to access and change this field.
+    # The R128_Track/Album_Gain fields are used to DERIVE the value of the gain from the
+    # afformentioned output gain field. The output gain stores the actual DISC replay gain.
+    # Then, to get the TRACK gain, you take the difference of the output gain fields and the
+    # R128_TRACK_GAIN field. In most cases, the R128_ALBUM_GAIN field is not used.
+    # I've tried just setting the Track Gain field regardless without having access to the
+    # ID headers, to no avail. All in all, such a stupid, unintuitive rediculous design choice.
+    # I have no idea why you would go against what literally every other codec does just to be
+    # "fancy". Dumb. This could be fixed, but you'd have to use another library that allows for
+    # the setting of the ID headers. 
+    
+    def getReplayGain(self):
+        return 0
+    def setReplayGain(self, value):
+        pass
 
-    # def getReplayGain(self):
-    #     replayGain = self.getMetadataField("R128_TRACK_GAIN")
-    #     return 0 if replayGain == "" else self.convertReplayGainToFloat(replayGain)
-    # def setReplayGain(self, value):
-    #     self.setMetadataField("R128_TRACK_GAIN", self.convertFloatToReplayGain(value))
-    # def convertReplayGainToFloat(self, value):
-    #     try:
-    #         return float(value.split()[0])
-    #     except:
-    #         return 0
-    # def convertFloatToReplayGain(self, value):
-    #     try:
-    #         return str(int(value * 256))
-    #     except:
-    #         return "0"
+
+class MP4File(SongFile):
+    def __init__(self, fileLocation):
+        self.file = MP4(fileLocation)
+        
+    def splitTrackDiskInteger(self, value, space):
+        try:
+            return value[space]
+        except:
+            return 0
+        
+    def getMP4FreeFormMetadataField(self, field):
+        try:
+            return self.getMetadataField(field).decode("UTF-8")
+        except:
+            return ""
+
+    def getTrackNumberCurrent(self):
+        return self.splitTrackDiskInteger(self.getMetadataField("trkn"), 0)
+    def setTrackNumberCurrent(self, value):
+        self.setMetadataField("trkn", [(value, self.getTrackNumberMaximum())])
+    
+    def getTrackNumberMaximum(self):
+        return self.splitTrackDiskInteger(self.getMetadataField("trkn"), 1)
+    def setTrackNumberMaximum(self, value):
+        self.setMetadataField("trkn", [(self.getTrackNumberCurrent(), value)])
+    
+    def getDiskNumberCurrent(self):
+        return self.splitTrackDiskInteger(self.getMetadataField("disk"), 0)
+    def setDiskNumberCurrent(self, value):
+        self.setMetadataField("disk", [(value, self.getDiskNumberMaximum())])
+    
+    def getDiskNumberMaximum(self):
+        return self.splitTrackDiskInteger(self.getMetadataField("disk"), 1)
+    def setDiskNumberMaximum(self, value):
+        self.setMetadataField("disk", [(self.getDiskNumberCurrent(), value)])
+
+    def getTitle(self):
+        return self.getMetadataField("\xa9nam")
+    def setTitle(self, value):
+        self.setMetadataField("\xa9nam", value)
+
+    def getArtist(self):
+        return self.getMetadataField("\xa9ART")
+    def setArtist(self, value):
+        self.setMetadataField("\xa9ART", value)
+
+    def getAlbum(self):
+        return self.getMetadataField("\xa9alb")
+    def setAlbum(self, value):
+        self.setMetadataField("\xa9alb", value)
+
+    def getDate(self):
+        return self.getMetadataField("\xa9day")
+    def setDate(self, value):
+        self.setMetadataField("\xa9day", value)
+
+    def getGenre(self):
+        return self.getMetadataField("\xa9gen")
+    def setGenre(self, value):
+        self.setMetadataField("\xa9gen", value)
+
+    def getComposer(self):
+        return self.getMetadataField("\xa9wrt")
+    def setComposer(self, value):
+        self.setMetadataField("\xa9wrt", value)
+
+    def getURL(self):
+        return self.getMP4FreeFormMetadataField("----:com.apple.iTunes:WWW")
+
+    def setURL(self, value):
+        self.setMetadataField("----:com.apple.iTunes:WWW", value.encode("UTF-8"))
+
+    def getReplayGain(self):
+        replayGain = self.getMP4FreeFormMetadataField("----:com.apple.iTunes:replaygain_track_gain")
+        return 0 if replayGain == "" else self.convertReplayGainToFloat(replayGain)
+    def setReplayGain(self, value):
+        self.setMetadataField("----:com.apple.iTunes:replaygain_track_gain", self.convertFloatToReplayGain(value).encode("UTF-8"))
+
+    def getComment(self):
+        return self.getMetadataField("\xa9cmt")
+    def setComment(self, value):
+        self.setMetadataField("\xa9cmt", value)
+
+    def getDescription(self):
+        return self.getMetadataField("desc")
+    def setDescription(self, value):
+        self.setMetadataField("desc", value)
 
 
 class MP3File(SongFile):
@@ -282,54 +357,4 @@ class MP3File(SongFile):
         return self.getMetadataField("TIT1")
     def setDescription(self, value):
         self.setMetadataField(TIT1(text=value))
-
-
-class MP4File(SongFile):
-    def __init__(self, fileLocation):
-        self.file = MP4(fileLocation)
-
-    def getTitle(self):
-        return self.getMetadataField("\xa9nam")
-    def setTitle(self, value):
-        self.setMetadataField("\xa9nam", value)
-
-    def getArtist(self):
-        return self.getMetadataField("\xa9ART")
-    def setArtist(self, value):
-        self.setMetadataField("\xa9ART", value)
-
-    def getAlbum(self):
-        return self.getMetadataField("\xa9alb")
-    def setAlbum(self, value):
-        self.setMetadataField("\xa9alb", value)
-
-    def getDate(self):
-        return self.getMetadataField("\xa9day")
-    def setDate(self, value):
-        self.setMetadataField("\xa9day", value)
-
-    def getGenre(self):
-        return self.getMetadataField("\xa9gen")
-    def setGenre(self, value):
-        self.setMetadataField("\xa9gen", value)
-
-    def getComposer(self):
-        return self.getMetadataField("\xa9wrt")
-    def setComposer(self, value):
-        self.setMetadataField("\xa9wrt", value)
-
-    def getURL(self):
-        return self.getMetadataField("purl")
-    def setURL(self, value):
-        self.setMetadataField("purl", value)
-
-    def getDescription(self):
-        return self.getMetadataField("desc")
-    def setDescription(self, value):
-        self.setMetadataField("desc", value)
-
-    def getComment(self):
-        return self.getMetadataField("\xa9cmt")
-    def setComment(self, value):
-        self.setMetadataField("\xa9cmt", value)
 
